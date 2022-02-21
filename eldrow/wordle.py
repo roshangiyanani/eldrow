32dcf9400e099df556f0e9d8c548d3ae7e1873c0
Roshan Giyanani <roshangiyanani@gmail.com>
2022-02-21 14:00:29 -0500

    shorten CharacterResult name

--- eldrow/wordle.py
+++ eldrow/wordle.py
@@ -3,13 +3,13 @@ from enum import Enum
 from typing import List, Sequence
 
 
-class CharacterResult(Enum):
+class CharResult(Enum):
     Gray = "X"
     Yellow = "Y"
     Green = "G"
 
     @staticmethod
-    def to_string(results: Sequence["CharacterResult"]) -> str:
+    def to_string(results: Sequence["CharResult"]) -> str:
         return "".join((r.value for r in results))
 
 
@@ -21,7 +21,7 @@ class Wordle:
         self._word = word.casefold()
         self._character_counts = Counter(self._word)
 
-    def make_guess(self, guess: str) -> List[CharacterResult]:
+    def make_guess(self, guess: str) -> List[CharResult]:
         if not guess:
             raise ValueError("cannot have an empty guess")
 
@@ -30,19 +30,19 @@ class Wordle:
         if len(guess) != len(self._word):
             raise ValueError("guess does not match word length")
 
-        result = [CharacterResult.Gray for _ in range(len(self._word))]
+        result = [CharResult.Gray for _ in range(len(self._word))]
 
         guess_character_counts = Counter(guess)
         overlap = self._character_counts & guess_character_counts
 
         for index, (correct, guessed) in enumerate(zip(self._word, guess)):
             if correct == guessed:
-                result[index] = CharacterResult.Green
+                result[index] = CharResult.Green
                 overlap[correct] -= 1
 
         for index, (correct, guessed) in enumerate(zip(self._word, guess)):
-            if result[index] is not CharacterResult.Green and overlap.get(guessed):
-                result[index] = CharacterResult.Yellow
+            if result[index] is not CharResult.Green and overlap.get(guessed):
+                result[index] = CharResult.Yellow
                 overlap[guessed] -= 1
 
         return result
--- tests/test_wordle.py
+++ tests/test_wordle.py
@@ -1,57 +1,57 @@
-from eldrow.wordle import Wordle, CharacterResult
+from eldrow.wordle import Wordle, CharResult
 
 
 def test_correct():
     w = Wordle("ABATE")
 
     result = w.make_guess("abate")
-    assert all(map(lambda r: r == CharacterResult.Green, result))
+    assert all(map(lambda r: r == CharResult.Green, result))
 
     result = w.make_guess("ABATE")
-    assert all(map(lambda r: r == CharacterResult.Green, result))
+    assert all(map(lambda r: r == CharResult.Green, result))
 
     result = w.make_guess("AbAtE")
-    assert all(map(lambda r: r == CharacterResult.Green, result))
+    assert all(map(lambda r: r == CharResult.Green, result))
 
 
 def test_incorrect():
     w = Wordle("ABATE")
 
     result = w.make_guess("FLOWN")
-    assert all(map(lambda r: r == CharacterResult.Gray, result))
+    assert all(map(lambda r: r == CharResult.Gray, result))
 
 
 def test_greens():
     w = Wordle("ABATE")
 
     result = w.make_guess("FLAKE")
-    assert CharacterResult.to_string(result) == "XXGXG"
+    assert CharResult.to_string(result) == "XXGXG"
 
     result = w.make_guess("ABACK")
-    assert CharacterResult.to_string(result) == "GGGXX"
+    assert CharResult.to_string(result) == "GGGXX"
 
     result = w.make_guess("ABASE")
-    assert CharacterResult.to_string(result) == "GGGXG"
+    assert CharResult.to_string(result) == "GGGXG"
 
     result = w.make_guess("ELATE")
-    assert CharacterResult.to_string(result) == "XXGGG"
+    assert CharResult.to_string(result) == "XXGGG"
 
 
 def test_yellows():
     w = Wordle("ABATE")
 
     result = w.make_guess("LATER")
-    assert CharacterResult.to_string(result) == "XYYYX"
+    assert CharResult.to_string(result) == "XYYYX"
 
     result = w.make_guess("METER")
-    assert CharacterResult.to_string(result) == "XYYXX"
+    assert CharResult.to_string(result) == "XYYXX"
 
 
 def test_greens_and_yellows():
     w = Wordle("ABATE")
 
     result = w.make_guess("ABBEY")
-    assert CharacterResult.to_string(result) == "GGXYX"
+    assert CharResult.to_string(result) == "GGXYX"
 
     result = w.make_guess("AGORA")
-    assert CharacterResult.to_string(result) == "GXXXY"
+    assert CharResult.to_string(result) == "GXXXY"
