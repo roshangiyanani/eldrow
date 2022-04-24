0f9e1f3ee22169ee41afc96f32f7e5cf7db2e411
Roshan Giyanani <roshangiyanani@gmail.com>
2022-04-24 15:38:24 -0400

    add dumb game run to main

--- eldrow/__main__.py
+++ eldrow/__main__.py
@@ -1,7 +1,9 @@
-from pathlib import Path
-from eldrow.lib import load_words
-
 import logging
+from pathlib import Path
+import random
+
+from eldrow.lib import load_words
+from eldrow.wordle import CharResult, Wordle
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -9,3 +11,16 @@ logger = logging.getLogger()
 word_path = Path("./wordlist.csv")
 words = load_words(word_path)
 logging.info(f"loaded {len(words)} words from {word_path}")
+
+random_word = random.choice(words)
+logging.info(f'randomly chose: "{random_word}"')
+wordle = Wordle(random_word)
+consideration_set = set(words)
+
+count = 0
+for word in consideration_set:
+    count += 1
+    result = wordle.make_guess(word)
+    print(f"({count}) {word}: {''.join(cr.value for cr in result)}")
+    if all(cr == CharResult.Green for cr in result):
+        break
